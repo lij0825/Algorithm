97bb20defc1137eded1b9a567a9c0bc24146566d
이인준 <72695551+lij0825@users.noreply.github.com>
2024-03-30 15:11:34 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 132 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -6,27 +6,28 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int[] dp;
+	static int v, c, k, M;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken()); // 개수
-		int M = Integer.parseInt(st.nextToken()); // 제한 무게
+		M = Integer.parseInt(st.nextToken()); // 제한 무게
 
 		dp = new int[M + 1];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
-			knapsack(v, c, k, M);
+			v = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			knapsack();
 		}
 
 		System.out.println(dp[M]);
 	}
 
-	static void knapsack(int v, int c, int k, int M) {
+	static void knapsack() {
 		if (v * k >= M) {
 			for (int j = v; j <= M; j++) {
 				dp[j] = Math.max(dp[j], dp[j - v] + c);
