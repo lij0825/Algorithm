7df14de8a5cca5fd7497a3a940d4503ab1b530df
이인준 <72695551+lij0825@users.noreply.github.com>
2026-08-03 15:41:43 +0900

    Time: 10 ms (83.24%), Space: 85.5 MB (65.91%) - LeetHub

---
+++ 1522-stone-game-iii/1522-stone-game-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = 0;
+            dp[i] = Integer.MIN_VALUE;
+
+            for (int k = 1; k <= 3 && i + k <= n; k++) {
+                sum += stoneValue[i + k - 1];
+                dp[i] = Math.max(dp[i], sum - dp[i + k]);
+            }
+        }
+
+        return dp[0] > 0 ? "Alice" : (dp[0] < 0 ? "Bob" : "Tie");
+    }
+}
